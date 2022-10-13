a70ccdb7281e237e24fc5bf20d2b6f554cc6f2b6
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-10-13 12:56:55 +0100

    Add to favourites logic completed; Dummy favourites page.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -24,6 +24,12 @@ const MovieListPage = (props) => {
     if (type === "name") setNameFilter(value);
     else setGenreFilter(value);
   };
+  const addToFavourites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favourite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
 
   useEffect(() => {
     fetch(
@@ -53,7 +59,7 @@ const MovieListPage = (props) => {
       genreFilter={genreFilter}
     />
         </Grid>
-        <MovieList movies={displayedMovies} />
+        <MovieList movies={displayedMovies} selectFavourite={addToFavourites} />
       </Grid>
     </Grid>
   );
