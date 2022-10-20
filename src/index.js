767442d648a114e85513b28e8b9db0aa41d2cd02
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-10-20 11:52:58 +0100

    Change site header.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {createRoot} from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
+import SiteHeader from './components/siteHeader'
 import favouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
 import {Link} from 'react-router-dom'
@@ -10,14 +11,7 @@ import {Link} from 'react-router-dom'
 const App = () => {
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies/favourites">Favourites</Link>
-        </li>
-      </ul>
+          <SiteHeader />      {/* New Header  */}
       <Routes>
         <Route exact path="/movies/favourites" element={<favouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
